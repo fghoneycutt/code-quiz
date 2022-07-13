c634fa8196fdd05755b4270a176ee6cce4472905
fghoneycutt <fghoneycutt@gmail.com>
2022-07-13 01:31:11 -0400

    added game over screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,11 @@ var questionBankObj = [
   },
   {
     question: "Arrays in JavaScript can be used to store ________.",
-    choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above",
-    ],
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above",],
     answer: 3,
   },
   {
-    question:
-      "The condition in an if/else statement is enclosed with ________.",
+    question: "The condition in an if/else statement is enclosed with ________.",
     choices: ["quotes", "parenthesis", "curly brackets", "square brackets"],
     answer: 1,
   },
@@ -38,18 +32,19 @@ var startQuiz = document.querySelector("#startBtn");
 var body = document.body;
 var questionNumber = 0;
 //Start countdown and quiz after button is clicked
-var timeLeft = 60;
 startQuiz.addEventListener("click", function () {
   countdown();
 });
 var timerEl = document.getElementById("countdown");
+var timeLeft = 60;
 function countdown() {
   var timeInterval = setInterval(function () {
-    if (timeLeft > 1) {
+    if (timeLeft > -1) {
       timerEl.textContent = "Time/Score: " + timeLeft;
       timeLeft--;
     } else {
-      timerEl.textContent = "Time's Up!";
+      clearInterval(timeInterval);
+      return;
     }
   }, 1000);
 }
@@ -89,56 +84,110 @@ var createQuizStructure = function () {
   nextQuestion();
 };
 var nextQuestion = function () {
+  //identify the question and the answer buttons to populate with text content
   var question = document.querySelector("#question");
   var button1 = document.querySelector("#button1");
   var button2 = document.querySelector("#button2");
   var button3 = document.querySelector("#button3");
   var button4 = document.querySelector("#button4");
+  //populate question and answers with text from questionBankObj
   question.textContent = questionBankObj[questionNumber].question;
   button1.textContent = questionBankObj[questionNumber].choices[0];
   button2.textContent = questionBankObj[questionNumber].choices[1];
   button3.textContent = questionBankObj[questionNumber].choices[2];
   button4.textContent = questionBankObj[questionNumber].choices[3];
 };
+//check if button1 was correct
 var button1Clicked = function(){
   if (button1.textContent === questionBankObj[questionNumber].choices[questionBankObj[questionNumber].answer]){
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   } else {
     timeLeft-= 10;
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   }
 }
+//check if button2 was correct
 var button2Clicked = function(){
   if (button2.textContent === questionBankObj[questionNumber].choices[questionBankObj[questionNumber].answer]) {
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   } else {
     timeLeft -= 10;
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   }
 }
+//check if button3 was correct
 var button3Clicked = function(){
   if (button3.textContent === questionBankObj[questionNumber].choices[questionBankObj[questionNumber].answer]) {
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   } else {
     timeLeft -= 10;
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   }
 }
+//check if button4 was correct
 var button4Clicked = function(){
   if (button4.textContent === questionBankObj[questionNumber].choices[questionBankObj[questionNumber].answer]) {
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   } else {
     timeLeft -= 10;
     questionNumber++;
+    if (questionNumber == questionBankObj.length) {
+      displayGameOver();
+    } 
     nextQuestion();
   }
 }
 
+//remove question content and build final score submittal 
+var displayGameOver = function(){
+  var score = timeLeft;
+  console.log(score);
+  var elem = document.getElementById("container");
+  elem.remove();
+  timerEl.remove();
+  var doneScreen = document.createElement("div");
+  body.appendChild(doneScreen);
+  var h1Elem = document.createElement("h1");
+  h1Elem.textContent = "All done!"
+  doneScreen.appendChild(h1Elem);
+  var h2Elem = document.createElement("h2");
+  h2Elem.textContent = "Your final score is " + score;
+  doneScreen.appendChild(h2Elem);
+}
+
+var displayHighScores = function(){
+
+  var scoreScreen = document.createElement("div");
+  
+}
+
 startQuiz.addEventListener("click", createQuizStructure);
+viewHighScores.addEventListener("click", displayHighScores);
